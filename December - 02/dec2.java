e86b1379edceac5a422bd814f7f75e7c8f9736c1
VADLA-CHARY <76141959+VADLA-CHARY@users.noreply.github.com>
2022-12-12 02:35:42 +0530

    Create dec2.java
    input format 
    only one line of input takes string and outputs string

---
+++ December - 02/dec2.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan= new Scanner(System.in);
+		
+		String string= scan.next();
+		int l=string.length();
+		
+		StringBuilder sb= new StringBuilder(string);
+		sb.delete(l-2,l);
+		
+		String prefix=sb.substring(l-4,l-2);
+		sb.delete(l-4,l-2);
+		
+		StringBuilder xy=new StringBuilder(prefix);
+		xy.append(sb);
+		
+		System.out.println(xy);
+	
+	}
+}
