6b54885f8a87fd462c06e48c65599b3ac10c4253
VADLACHARY <76141959+vadlachary2002@users.noreply.github.com>
2022-12-15 16:46:57 +0530

    Create java_charyvadla2002_KochousephKonundrum.java
    input format 
    first line contains N no of word
    Next line contains N  separate spaced words

---
+++ December - 09/java_charyvadla2002_KochousephKonundrum.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class java_charyvadla2002_KochousephKonundrum {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int n=scan.nextInt();
+        HashMap<Integer,String> hash = new HashMap<>();
+        for(int i=0;i<n;i++){
+            String x=scan.next();
+            int len=x.length();
+            if(!hash.containsKey(len)){
+                hash.put(len,x);
+            }
+        }
+        int max=-1,ckey=-1;
+        for(int key:hash.keySet()){
+            if(key%2==1 && max<key){
+                ckey=key;
+                max=key;
+            }
+        }
+        if(ckey==-1){
+            System.out.println("Better luck next time");
+        }else{
+            System.out.println("Output: "+hash.get(ckey));
+        }
+
+    }
+}
